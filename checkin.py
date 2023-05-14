6e99fe6adf8469522642484eb475f49446e14a38
mjyp207 <129351082+mjyp207@users.noreply.github.com>
2023-05-14 19:41:37 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -22,8 +22,8 @@ B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 s = requests.Session()
  
 # 在下面两行的引号内贴上账号（仅支持手机号）和密码
-username = ""
-password = ""
+username = "17793732161"
+password = "Pip@83126"
  
 _ = """
 if(username == "" or password == ""):
