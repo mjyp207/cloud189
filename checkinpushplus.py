11078a3fc6621da412e686f78981ef973f20f2e3
mjyp207 <129351082+mjyp207@users.noreply.github.com>
2023-05-15 21:41:53 +0800

    Update checkinpushplus.py

--- checkinpushplus.py
+++ checkinpushplus.py
@@ -3,207 +3,231 @@ import re
 import json
 import base64
 import hashlib
-# from urllib import parse
-import urllib.parse,hmac
-import rsa
+from logging import getLogger, StreamHandler, FileHandler, Formatter
+from typing import Dict
+from contextlib import contextmanager
+from pathlib import Path
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicNumbers
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 import requests
-import random
-
-s = requests.Session()
-
-username = ""
-password = ""
-PUSHPLUS_TOKEN = ""
-# pushplus推送 设置密钥
  
-if not pushplus_token:
-    print("第16行的pushplus_token 为空，签到结果将不会推送")
-
-if(username == "" or password == ""):
-    username = input("账号：")
-    password = input("密码：")
-   
-   
-def main():
-    s=login(username, password)
-    rand = str(round(time.time() * 1000))
-    surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
-    url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
-    url2 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN'
-    url3 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_2022_FLDFS_KJ&activityId=ACT_SIGNIN'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
-        "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
-        "Host": "m.cloud.189.cn",
-        "Accept-Encoding": "gzip, deflate",
-    }
-    response = s.get(surl, headers=headers)
-    netdiskBonus = response.json()['netdiskBonus']
-    if (response.json()['isSign'] == "false"):
-        print(f"未签到，签到获得{netdiskBonus}M空间")
-        res1 = f"未签到，签到获得{netdiskBonus}M空间"
-    else:
-        print(f"已经签到过了，签到获得{netdiskBonus}M空间")
-        res1 = f"已经签到过了，签到获得{netdiskBonus}M空间"
  
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
-        "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
-        "Host": "m.cloud.189.cn",
-        "Accept-Encoding": "gzip, deflate",
-    }
-    response = s.get(url, headers=headers)
-    if ("errorCode" in response.text):
-        print(response.text)
-        res2 = ""
-    else:
-        description = response.json()['description']
-        print(f"抽奖获得{description}")
-        res2 = f"抽奖获得{description}"
-    response = s.get(url2, headers=headers)
-    if ("errorCode" in response.text):
-        print(response.text)
-        res3 = ""
-    else:
-        description = response.json()['description']
-        print(f"抽奖获得{description}")
-        res3 = f"抽奖获得{description}"
- 
-    response = s.get(url3, headers=headers)
-    if ("errorCode" in response.text):
-        print(response.text)
-        res4 = ""
-    else:
-        description = response.json()['description']
-        print(f"链接3抽奖获得{description}")
-        res4 = f"链接3抽奖获得{description}"
-        
-    if pushplus_token:
-                title = '天翼云盘签到'
-                url = 'http://www.pushplus.plus/send'
-                data = {
-                    "token": pushplus_token,
-                    "title": title,
-                    "content": f'{username}\n{res1}\n{res2}\n{res3}\n{res4}\n',
-                }
+# 常量定义
 BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
-def int2char(a):
+B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+ 
+ 
+# 定义日志系统
+logger = getLogger(__name__)
+logger.setLevel('INFO')
+log_format = Formatter('[%(asctime)s] %(levelname)s : %(message)s')
+ 
+console_handler = StreamHandler()
+console_handler.setLevel('INFO')
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
+ 
+log_path = Path('logs')
+log_path.mkdir(parents=True, exist_ok=True)
+log_file = log_path / f'{time.strftime("%Y%m%d")}.log'
+file_handler = FileHandler(log_file, mode='a', encoding='utf-8')
+file_handler.setLevel('DEBUG')
+file_handler.setFormatter(log_format)
+logger.addHandler(file_handler)
+ 
+ 
+# 定义工具函数
+def int2char(a: int) -> str:
     return BI_RM[a]
  
-B64MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"  
-def b64tohex(a):
-    d = ""
-    e = 0
-    c = 0
-    for i in range(len(a)):
-        if list(a)[i] != "=":
-            v = B64MAP.index(list(a)[i])
-            if 0 == e:
-                e = 1
-                d += int2char(v >> 2)
-                c = 3 & v
-            elif 1 == e:
-                e = 2
-                d += int2char(c << 2 | v >> 4)
-                c = 15 & v
-            elif 2 == e:
-                e = 3
-                d += int2char(c)
-                d += int2char(v >> 2)
-                c = 3 & v
-            else:
-                e = 0
-                d += int2char(c << 2 | v >> 4)
-                d += int2char(15 & v)
-    if e == 1:
-        d += int2char(c << 2)
-    return d
+ 
+def b64tohex(a: str) -> str:
+    a += '=='[(len(a) % 4):]
+    data = base64.b64decode(a)
+    return data.hex()
  
  
-def rsa_encode(j_rsakey, string):
-    rsa_key = f"-----BEGIN PUBLIC KEY-----\n{j_rsakey}\n-----END PUBLIC KEY-----"
-    pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(rsa_key.encode())
-    result = b64tohex((base64.b64encode(rsa.encrypt(f'{string}'.encode(), pubkey))).decode())
+def sign_params(params: str, secret_key: str) -> str:
+    sign = f"{params}{secret_key}"
+    md5 = hashlib.md5(sign.encode('utf-8')).hexdigest()
+    return md5
+ 
+ 
+def encode_rsa_public_key(n: int, e: int) -> str:
+    public_numbers = RSAPublicNumbers(e, n)
+    public_key = public_numbers.public_key(backend=default_backend())
+    pem = public_key.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+    pem = pem.decode().strip().split('\n')[1:-1]
+    pem = ''.join(pem)
+    return pem
+ 
+ 
+def rsa_encode(j_rsakey: str, string: str) -> str:
+    numbers = json.loads(j_rsakey)
+    public_key = encode_rsa_public_key(numbers['n'], numbers['e'])
+    public_key = serialization.load_pem_public_key(public_key.encode(), backend=default_backend())
+    ciphertext = public_key.encrypt(string.encode(), padding.PKCS1v15())
+    result = ciphertext.hex()
     return result
  
  
-def calculate_md5_sign(params):
-    return hashlib.md5('&'.join(sorted(params.split('&'))).encode('utf-8')).hexdigest()
+# 定义登陆函数
+def login(username: str, password: str, timeout: int = 10) -> requests.Session:
+    url_token = "https://m.cloud.189.cn/udb/udb_login.jsp?pageId=1&pageKey=default&clientType=wap&redirectURL=https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html"
+    url_contacts = "https://api.cloud.189.cn/mkt/userSign.action?rand=0.34093141047311375"
  
- 
-def login(username, password):
-    #https://m.cloud.189.cn/login2014.jsp?redirectURL=https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html
-    url=""
-    urlToken="https://m.cloud.189.cn/udb/udb_login.jsp?pageId=1&pageKey=default&clientType=wap&redirectURL=https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html"
-    s = requests.Session()
-    r = s.get(urlToken)
-    pattern = r"https?://[^\s'\"]+"  # 匹配以http或https开头的url
-    match = re.search(pattern, r.text)  # 在文本中搜索匹配
-    if match:  # 如果找到匹配
-        url = match.group()  # 获取匹配的字符串
-        # print(url)  # 打印url
-    else:  # 如果没有找到匹配
-        print("没有找到url")
- 
-    r = s.get(url)
-    # print(r.text)
-    pattern = r"<a id=\"j-tab-login-link\"[^>]*href=\"([^\"]+)\""  # 匹配id为j-tab-login-link的a标签，并捕获href引号内的内容
-    match = re.search(pattern, r.text)  # 在文本中搜索匹配
-    if match:  # 如果找到匹配
-        href = match.group(1)  # 获取捕获的内容
-        # print("href:" + href)  # 打印href链接
-    else:  # 如果没有找到匹配
-        print("没有找到href链接")
- 
-    r = s.get(href)
-    captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
-    lt = re.findall(r'lt = "(.+?)"', r.text)[0]
-    returnUrl = re.findall(r"returnUrl= '(.+?)'", r.text)[0]
-    paramId = re.findall(r'paramId = "(.+?)"', r.text)[0]
-    j_rsakey = re.findall(r'j_rsaKey" value="(\S+)"', r.text, re.M)[0]
-    s.headers.update({"lt": lt})
- 
-    username = rsa_encode(j_rsakey, username)
-    password = rsa_encode(j_rsakey, password)
-    url = "https://open.e.189.cn/api/logbox/oauth2/loginSubmit.do"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
-        'Referer': 'https://open.e.189.cn/',
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-G9750 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.61 Mobile Safari/537.36 MCloudApp/7.4.0',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Referer': 'https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html'
     }
-    data = {
-        "appKey": "cloud",
-        "accountType": '01',
-        "userName": f"{{RSA}}{username}",
-        "password": f"{{RSA}}{password}",
-        "validateCode": "",
-        "captchaToken": captchaToken,
-        "returnUrl": returnUrl,
-        "mailSuffix": "@189.cn",
-        "paramId": paramId
+ 
+    # 获取加密参数
+    try:
+        resp = requests.get(url_token, headers=headers, timeout=timeout)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException:
+        logger.exception('Failed to get token parameters')
+        raise
+ 
+    params_str = re.findall(r'params: \'(.+)\',', resp.text)[0]
+    secret_key = re.findall(r'secretKey: \'(.+)\',', resp.text)[0]
+    j_rsakey = re.findall(r'j_rsakey" value=\'(.+)\'>', resp.text)[0]
+    sid = re.findall(r"mCloud_sid = '(.+)';", resp.text)[0]
+ 
+    # 加密账号和密码
+    params = {
+        'appId': '',
+        'accountType': '01',
+        'userName': rsa_encode(j_rsakey, username),
+        'password': rsa_encode(j_rsakey, password),
+        'validateCode': '',
+        'smsCode': '',
+        'mailCode': '',
+        'isOauth2': 'false',
+        'clientType': 'android',
+        'cb_SaveName': '0',
+        'mailSuffix': '@189.cn',
+        'deviceId': '',
+        'umidToken': '',
+        'tkFlag': 'true',
+        'returnUrl': '',
+        'showType': '03',
+        'redirectURL': 'https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html',
+        'state': '',
+        'param': '',
+        'hdfs_device_id': '',
+        'mCloud_sid': sid
     }
-    r = s.post(url, data=data, headers=headers, timeout=5)
-    if (r.json()['result'] == 0):
-        print(r.json()['msg'])
+ 
+    params['sign'] = sign_params(json.dumps(params, separators=(',', ':')), secret_key)
+ 
+    # 登录
+    session = requests.session()
+    try:
+        resp = session.post(url_token, headers=headers, data=base64.b64encode(json.dumps(params).encode()).decode(), timeout=timeout)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException:
+        logger.exception('Failed to login')
+        raise
+     
+    if resp.json().get('result') != '0':
+        logger.error('Failed to login: %s', resp.json())
+        raise RuntimeError('Failed to login')
+     
+    logger.info('Login successfully')
+ 
+    return session
+ 
+ 
+# 定义签到函数
+def sign_in(session: requests.Session, timeout: int = 10) -> Dict[str, int]:
+    url_sign_in = "https://api.cloud.189.cn/mkt/userSign.action?rand=0.34093141047311375"
+    url_lottery = "https://api.cloud.189.cn/mkt/lottery.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN"
+    url_reward_info = "https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=21&activityId=ACT_SIGNIN"
+    url_message = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ENTER_YOUR_KEY"
+ 
+    headers = {
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-G9750 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.61 Mobile Safari/537.36 MCloudApp/7.4.0',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Referer': 'https://m.cloud.189.cn/zhuanti/2021/shakeLottery/index.html'
+    }
+ 
+    # 签到
+    try:
+        resp = session.get(url_sign_in, headers=headers, timeout=timeout)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException:
+        logger.exception('Failed to sign in')
+        raise
+ 
+    resp_json = resp.json()
+    if resp_json.get('isSign') == 'Y':
+        logger.info('Already signed in')
+    elif resp_json.get('signFlag') == '1':
+        logger.info('Signed in successfully')
     else:
-        print(r.json()['msg'])
-    redirect_url = r.json()['toUrl']
-    r = s.get(redirect_url)
-    return s
+        msg = f"Failed to sign in: {resp_json.get('msg')}"
+        logger.warning(msg)
+ 
+    # 抽奖
+    rewards = {}
+    for i in range(3):
+        try:
+            resp_lottery = session.get(url_lottery, headers=headers, timeout=timeout)
+            resp_lottery.raise_for_status()
+        except requests.exceptions.RequestException:
+            logger.exception('Failed to draw lottery')
+            raise
+         
+        resp_reward_info = session.get(url_reward_info, headers=headers, timeout=timeout)
+        resp_reward_info.raise_for_status()
+ 
+        resp_json = resp_reward_info.json()
+        name = resp_json['giftList'][i]['giftName']
+        num = int(resp_json['giftList'][i]['giftNum'])
+        rewards[name] = num
+ 
+        logger.info('Draw lottery: %s x %d', name, num)
+ 
+    # 发送企业微信消息
+    message = f"天翼云盘签到：已签到\n\n"
+    for name, num in rewards.items():
+        message += f"- {name} x {num}\n"
+    message = message.strip()
+ 
+    params = {
+        "msgtype": "text",
+        "text": {
+            "content": message
+        }
+    }
+ 
+    try:
+        resp = requests.post(url_message, json=params, timeout=timeout)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException:
+        logger.exception('Failed to send wechat message')
+        raise
+ 
+    logger.info('Send wechat message successfully')
+ 
+    return rewards
  
  
-
-def lambda_handler(event, context):  # aws default
-    main()
+# 定义主函数
+def main(username: str, password: str, timeout: int = 10) -> None:
+    with login(username, password, timeout=timeout) as session:
+        rewards = sign_in(session, timeout=timeout)
+ 
+    logger.info('Sign in finish: %s', rewards)
  
  
-def main_handler(event, context):  # tencent default
-    main()
- 
- 
-def handler(event, context):  # aliyun default
-    main()
- 
- 
-if __name__ == "__main__":
-    # time.sleep(random.randint(5, 30))
-    main()
+if __name__ == '__main__':
+    username = ""
+    password = ""
+    main(username, password)
